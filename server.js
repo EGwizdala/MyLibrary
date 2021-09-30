97aff4e9ea7ca3f3e3a305904fa0b41cec04bdf7
EGwizdala <emilia.gwizdala@gmail.com>
2021-09-30 10:15:55 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,32 @@
+//access one specific variable, access it like any property of an object:
+// console.log('The value of PORT is:', process.env.PORT);
+//if checks if app is in production
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+  
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+
+ 
+
+const indexRouter = require('./routes/index')
+ 
+app.set('view engine', "ejs");
+app.set("views", __dirname + '/views');
+app.set('layout', "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static('public'));
+ 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on("error", error => console.error(error))
+db.once("open", () => console.log("conected to mongoose"))
+
+app.use('/', indexRouter)
+app.listen(process.env.PORT || 3000)
